6e72d962ec2ec3edec09c6200a024324b2be2abf
jaime-rey <jaimereycasado@gmail.com>
2023-08-06 00:07:30 +0200

    finish by the moment

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/**/*.html',
-      './node_modules/flowbite/**/*.js'
+    "./static/**/*.js",
+    "./templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-        require('flowbite/plugin')
-    ]
-}
-
+  plugins: [require("flowbite/plugin")],
+};
